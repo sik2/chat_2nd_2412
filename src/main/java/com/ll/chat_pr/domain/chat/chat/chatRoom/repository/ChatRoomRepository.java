294bdc379d137e2ab0e6acdbc936919a88418671
sik2 <bickwang123@gmail.com>
2024-12-31 17:51:33 +0900

    init

--- src/main/java/com/ll/chat_pr/domain/chat/chat/chatRoom/repository/ChatRoomRepository.java
+++
@@ -1,8 +0,0 @@
-package com.ll.chat_pr.domain.chat.chat.chatRoom.repository;
-
-import com.ll.chat_pr.domain.chat.chat.chatRoom.entity.ChatRoom;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
-
-}
